ac6e0e14adb2726cf9cf2c82837889c7e25a4367
Seokju Na <seokju.me@gmail.com>
2024-09-19 18:07:27 +0900

    fix

--- packages/yarn-plugin-deployer/sources/aws/clients.ts
+++ packages/yarn-plugin-deployer/sources/aws/clients.ts
@@ -1,5 +1,7 @@
 import { CloudFrontKeyValueStoreClient } from '@aws-sdk/client-cloudfront-keyvaluestore';
 import { S3Client } from '@aws-sdk/client-s3';
+import '@aws-sdk/signature-v4-crt';
+import { SignatureV4MultiRegion } from '@aws-sdk/signature-v4-multi-region';
 import memoize from 'memoize';
 
 export interface AwsClients {
@@ -17,7 +19,11 @@ interface Options {
 
 export const getAwsClients = memoize(({ region, credentials }: Options) => {
   const s3 = new S3Client({ region, credentials });
-  const cloudfrontKv = new CloudFrontKeyValueStoreClient({ region, credentials });
+  const cloudfrontKv = new CloudFrontKeyValueStoreClient({
+    region,
+    credentials,
+    signerConstructor: SignatureV4MultiRegion,
+  });
   const clients: AwsClients = { s3, cloudfrontKv };
   return clients;
 });
